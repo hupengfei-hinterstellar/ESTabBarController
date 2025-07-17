be2ad27e2042fafd4752fba184c12ff72155ea97
hupengfei <hupengfei@hinterstellar.com>
2025-07-17 14:17:11 +0800

    修改 package

--- Package.swift
+++ Package.swift
@@ -1,19 +1,23 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "ESTabBarController",
+    defaultLocalization: "en",
     platforms: [
-        .iOS(.v8),
+        .iOS(.v12),
     ],
     products: [
         .library(name: "ESTabBarController",  targets: ["ESTabBarController"])
     ],
     dependencies: [],
     targets: [
-        .target(name: "ESTabBarController", path: "Sources")
+        .target(
+            name: "ESTabBarController",
+            path: "Sources"
+            )
     ],
     swiftLanguageVersions: [.v5]
 )
